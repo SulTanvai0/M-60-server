aea0a6ce4a7806991eacd7ed2f1d87725e2f7b7e
SulTanvai0 <muhammedmehedih@gmail.com>
2023-01-10 07:18:23 +0600

    basic backend server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+const categories = require('./data/categories.json')
+
+app.get('/', (req, res) => {
+    res.send('news is here')
+})
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+app.listen(port, () => {
+    console.log('dargon news server running on ', port)
+})
